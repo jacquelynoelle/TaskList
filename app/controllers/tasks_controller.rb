a35d7e2d45474c71f117e903c9567ee2206b9cf5
jackie <jacquelyn.cheng@me.com>
2018-09-18 14:44:42 -0700

    Implemented Task model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,12 @@
-TASKS = [
-  { name: "CS Fun 1", description: "Complete reverse sentence", completion_date: "2018-09-21" },
-  { name: "Weekend Warrior", description: "Complete 05 Alouette", completion_date: "2018-09-24" },
-  { name: "Hotel", description: "Revisit and refactor", completion_date: "2018-10-01" },
-  { name: "POODR", description: "Read chapter 4", completion_date: "2018-10-01" },
-  { name: "Video", description: "Watch live Code of Job Board", completion_date: "2018-10-01" },
-  { name: "CS Fun 2", description: "Complete palindrome Check", completion_date: "2018-10-01" },
-  { name: "Optional: Ada Zoo", description: "Complete assignment", completion_date: "2018-10-01" }
-]
+# TASKS = [
+#   { name: "CS Fun 1", description: "Complete reverse sentence", completion_date: "2018-09-21" },
+#   { name: "Weekend Warrior", description: "Complete 05 Alouette", completion_date: "2018-09-24" },
+#   { name: "Hotel", description: "Revisit and refactor", completion_date: "2018-10-01" },
+#   { name: "POODR", description: "Read chapter 4", completion_date: "2018-10-01" },
+#   { name: "Video", description: "Watch live Code of Job Board", completion_date: "2018-10-01" },
+#   { name: "CS Fun 2", description: "Complete palindrome Check", completion_date: "2018-10-01" },
+#   { name: "Optional: Ada Zoo", description: "Complete assignment", completion_date: "2018-10-01" }
+# ]
 
 WEEK = "2018-09-24"
 
@@ -14,13 +14,15 @@ require 'date'
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all.order(:completion_date)
   end
 
   def show
     name = params[:name]
-    @task = TASKS.find do |task|
-      task[:name] == name
+    @task = Task.find_by(name: name)
+
+    if @task.nil?
+      render :notfound, status: :not_found
     end
   end
 end
